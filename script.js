a88c475831000c07a44ac19156ac25eed288c1e0
Reis Surdulli <reissurdulli9@gmail.com>
2025-09-25 20:44:22 +0200

    added module 27

---
+++ script.js
@@ -0,0 +1,25 @@
+function lum() {
+    alert("This is a function");
+}
+
+lum();
+
+
+function reis(number1, number2) {
+ return number1 + number2;
+};
+
+console.log(reis(10,10));
+
+function toCelsius(number1) {
+    return (number1 - 32) * 5 / 9;
+}
+
+console.log(toCelsius(56));
+
+function drin() {
+    var localvar = "this is local";
+    alert(localvar);
+}
+
+drin();
