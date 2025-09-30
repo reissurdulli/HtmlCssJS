4b2a58b7af5b24507d4678f0d341b0de8270ba11
Reis Surdulli <104998870+reissurdulli@users.noreply.github.com>
2025-09-30 20:48:32 +0200

    Update script.js

--- moduli 27/script.js
+++ moduli 27/script.js
@@ -1,25 +1,60 @@
-function lum() {
-    alert("This is a function");
+// function lum() {
+//     alert("This is a function");
+// }
+
+// lum();
+
+
+// function reis(number1, number2) {
+//  return number1 + number2;
+// };
+
+// console.log(reis(10,10));
+
+// function toCelsius(number1) {
+//     return (number1 - 32) * 5 / 9;
+// }
+
+// console.log(toCelsius(56));
+
+// function drin() {
+//     var localvar = "this is local";
+//     alert(localvar);
+// }
+
+// drin();
+
+var jora = () => alert("Hello Jora");
+jora();
+
+function toSeconds(num1) {
+    return (num1 *60);
 }
+console.log(toSeconds(2))
 
-lum();
-
-
-function reis(number1, number2) {
- return number1 + number2;
+var kerri = {
+  name: "audi",
+  color: "red",
+  year: 2016,
+  startEngine: function() {
+    alert("vroom");
+  }
 };
 
-console.log(reis(10,10));
+console.log(kerri.name);
+console.log(kerri.color);
+console.log(kerri.year);
 
-function toCelsius(number1) {
-    return (number1 - 32) * 5 / 9;
-}
+kerri.startEngine();
 
-console.log(toCelsius(56));
+var computer = new Object();
 
-function drin() {
-    var localvar = "this is local";
-    alert(localvar);
-}
+computer.name="Lenovo";
+computer.cpu="Intel Core i7";
+computer.ram="16GB";
+computer.gpu="GeForce 4080";
 
-drin();
+computer.type = function(){
+    return this.name+this.cpu+this.ram+this.gpu;
+};
+console.log(computer.type());
