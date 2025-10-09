f59914dea01d49289a4f02e6f5c41f6df67199d2
Reis Surdulli <104998870+reissurdulli@users.noreply.github.com>
2025-10-09 20:47:57 +0200

    moduli 29

---
+++ moduli29/main.js
@@ -0,0 +1,29 @@
+
+// var programmingLanguages = ['html','css','js'];
+// console.log(programmingLanguages);
+// console.log(programmingLanguages[2]);
+
+// programmingLanguages.push('reis')
+// console.log(programmingLanguages);
+
+// programmingLanguages.pop();
+// console.log(programmingLanguages);
+
+// programmingLanguages.shift('reis')
+// console.log(programmingLanguages);
+
+// programmingLanguages.unshift('reis')
+// console.log(programmingLanguages);
+
+// programmingLanguages.splice(2,1, 'react');
+// console.log(programmingLanguages); 
+
+console.log(Math.floor(Math.random()*10))
+
+var Students = ['Reis','Sufjan','Daors']
+
+var [s1,s2,s3] = Students;
+
+console.log(s1)
+
+console.log(s2)
