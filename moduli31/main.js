41b7e67315e0a54ba71eb8f0e81792d6fda6fbb6
Reis Surdulli <104998870+reissurdulli@users.noreply.github.com>
2025-10-21 20:44:38 +0200

    moduli 31

---
+++ moduli31/main.js
@@ -0,0 +1,77 @@
+// Do while loop
+var i = 0;
+
+
+do{
+    i = i + 1;
+    console.log(i);
+}while(i < 5);
+
+
+console.log("------------------------------")
+
+
+ // While loop
+
+
+ var j = 0;
+ while (j < 5){
+    j = j + 1;
+    console.log(j);
+ }
+
+
+ console.log("------------------------------")
+
+
+ let n = 0;
+ let x = 0;
+
+ while(n<3){
+    n++
+    x = x + n
+    console.log(n)
+    console.log(x)
+ }
+
+
+console.log("------------------------------")
+
+
+for(var i = 0; i<5; i++){
+    console.log("Value of i is: " +i)
+}
+
+
+console.log("------------------------------")
+
+
+var person = {firstName:"John", lastName: "Doe", age:"25"};
+
+var text = '';
+
+var z;
+
+for (z in person){
+    text += person[z];
+}
+console.log(text);
+
+console.log("------------------------------")
+
+var names = ['Steve', 'Bill', 'Mark'];
+var y;
+for(y of names){
+    console.log(y);
+}
+
+console.log("------------------------------")
+
+
+var txt = 'Javascript';
+
+var l;
+
+for (l of txt){
+    console.log(l)
+}
