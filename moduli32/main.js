970856d5cb266111d3d6db025014daef425e930c
Reis Surdulli <104998870+reissurdulli@users.noreply.github.com>
2025-10-23 20:45:15 +0200

    moduli 32

---
+++ moduli32/main.js
@@ -0,0 +1,27 @@
+// var text="The most famous school is Digital School";
+// var result=text.search("Digital School");
+// document.getElementById('result1').innerHTML = result;
+
+// var text="The most famous school is Digital School";
+// var result=text.replace(/Digital School/,"Ismail Qemaili");
+// document.getElementById('result2').innerHTML = result;
+
+// var text="reisdrinlumsufjan"
+// var regex = new RegExp('reis')
+// document.getElementById('result3').innerHTML = regex.test(text);
+
+// var text="My school is the best school of all school";
+// var regex = /school/g;
+// document.getElementById('result4').innerHTML = text.match(regex);
+
+// var text="My school is the best school of all school";
+// var regex = /s/g;
+// document.getElementById('result5').innerHTML = text.match(regex);
+
+// var text="My school is the best school of all school";
+// var regex = /[sol]/g;
+// document.getElementById('result6').innerHTML = text.match(regex);
+
+var text="My school is the top 10";
+var regex = /[0-9]/g;
+document.getElementById('result7').innerHTML = text.match(regex);
