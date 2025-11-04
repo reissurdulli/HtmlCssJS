e622ba5e3f90349b2a6d3490ac2e99e1855c3d4d
Reis Surdulli <104998870+reissurdulli@users.noreply.github.com>
2025-11-04 20:35:53 +0100

    Moduli 33

--- moduli33/main.js
+++ moduli33/main.js
@@ -15,3 +15,32 @@ if(name.match(valid_name_regex)){
     return false;
 }
 }
+
+
+function validation1(){
+var age= document.getElementById("age").value;
+
+
+var valid_age_regex = /^[0-9]+$/;
+
+if((age.match(valid_age_regex))){
+    return true;
+}else{
+    document.getElementById("age_error").style.visibility="visible";
+    document.getElementById("age").style.borderColor = "red";
+    return false;
+}
+}
+
+
+function validation2(){
+    var city = document.getElementById("city").value;
+
+    if(!city==""){
+        return true
+    }else{
+    document.getElementById("city_error").style.visibility="visible";
+    document.getElementById("city").style.borderColor = "red";
+    return false;
+    }
+}
